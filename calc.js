d765ba709303f8038d5621b49480eb1c9047a089
Tablesaw <ivanbure2005@gmail.com>
2024-10-03 21:05:17 +0300

    Validation fixed

--- calc.js
+++ calc.js
@@ -1,18 +1,18 @@
-
-window.addEventListener('DOMContentLoaded', function () {
-    const goodsPriceList={"product1": 100, "product2": 140, "product3":170, "product4": 80, "product5":125}
+window.addEventListener("DOMContentLoaded", function () {
+    const goodsPriceList =
+    {"pr1": 100, "pr2": 140, "pr3": 170, "pr4": 80, "pr5": 125};
 
     let amount = document.getElementById("amount");
     let product = document.getElementById("goodsList");
-    let calcButton=document.getElementById("calculate");
-    let resultfield=document.getElementById("result");
-    calcButton.addEventListener("click", ()=>{
-        let inp=amount.value;
-        if(inp.match(/^\d+$/)!==null){
-        let res=goodsPriceList[product.value] * inp;
-        resultfield.innerText=res;}
-        else{
-            resultfield.innerText="Некорректный ввод количества";
+    let calcButton = document.getElementById("calculate");
+    let resultfield = document.getElementById("result");
+    calcButton.addEventListener("click", function () {
+        let inp = amount.value;
+        if (inp.match(/^\d+$/) !== null) {
+            let res = goodsPriceList[product.value] * inp;
+            resultfield.innerText = res;
+        } else {
+            resultfield.innerText = "Некорректный ввод количества";
         }
     });
 });
